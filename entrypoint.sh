a7faa8a91d630c76fa4f597a06db0beb79995022
mydocker-repo <45119302@qq.com>
2025-11-26 09:27:08 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
 # entrypoint.sh
 
+# ssh
+mkdir -p /root/.ssh && chmod 700 /root/.ssh && \
+    touch /root/.ssh/known_hosts
+    
+# 生成密钥： ssh-keygen -t ed25519 -C "cron-container" -f ./id_cron
+# 使用已经生成的密钥，可以持久化保存（已经有公钥的远程服务器可以直接连接）
+if [ -f "/data/id_cron" ]; then
+    cp -a /data/id_cron* /root/.ssh/
+else
+    ssh-keygen -t ed25519 -N "" -C "cron-container" -f /root/.ssh/id_cron 
+    if [ -d "/data" ]; then
+        cp -a /root/.ssh/id_cron* /data/
+    fi
+if
+chmod 600 /root/.ssh/id_cron*
+
+cat <<EOF > /root/.ssh/config
+Host *
+    StrictHostKeyChecking no
+    IdentityFile /root/.ssh/id_cron
+EOF
 
 # 如果你把任务写在项目里的 crontab 文件，可以这样加载
 if [ -f "/data/crontab" ]; then
